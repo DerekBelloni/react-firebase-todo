dda78dc992d1b07aaddf97e4ce8e9946ccf55f6f
Derek <derekbelloni10@gmail.com>
2022-06-06 10:44:00 -0600

    initialized state in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Go to gym', 'Wash the car', 'Keep learning React'])
+
+
+
+
   return (
     <div className="App">
 
       <h1>Hello World!</h1>
       <input />
       <button>Add Todo</button>
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+
+      </ul>
     </div>
   );
 }
