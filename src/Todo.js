92012c3003db4404878adc83f1dd24156ff0656d
Derek <derekbelloni10@gmail.com>
2022-06-06 14:56:43 -0600

    modal integrated into todo component

--- src/Todo.js
+++ src/Todo.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import { Button, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material'
+import React, { useState } from 'react';
+import { Button, List, ListItem, ListItemAvatar, ListItemText, Modal } from '@mui/material'
 import './Todo.css'
 import { db } from './firebase'
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 
 const Todo = (props) => {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
   return (
-    <List>
-      <ListItem>
-        <ListItemAvatar>
-        </ListItemAvatar>
-        <ListItemText primary={props.todo.text} secondary="Dummy deadline" ></ListItemText>
-      </ListItem>
-      <DeleteForeverIcon onClick={event => { db.collection('todos').doc(props.todo.id).delete() }} />
-    </List>
+    <>
+      <Modal
+        open={open}
+        onClose={e => setOpen(false)}
+      >
+        <div>
+          <h1>open</h1>
+          <button onClick={e => setOpen(false)}>Close</button>
+        </div>
+      </Modal>
+      <List>
+        <ListItem>
+          <ListItemAvatar>
+          </ListItemAvatar>
+          <ListItemText primary={props.todo.text} secondary="Dummy deadline" ></ListItemText>
+        </ListItem>
+        <button onClick={e => setOpen(true)}>Edit</button>
+        <DeleteForeverIcon onClick={event => { db.collection('todos').doc(props.todo.id).delete() }} />
+      </List>
+    </>
   );
 }
 
