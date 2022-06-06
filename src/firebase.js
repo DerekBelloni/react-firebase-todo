5fa923b6888709319ed0a9d6bfe6329381432262
Derek <derekbelloni10@gmail.com>
2022-06-06 12:45:30 -0600

    firebase config setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/datastore'
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyASl1BmSbD8YXmIdslQt0L9gvClYz7bDrE",
+  authDomain: "react-firebase-todo-29447.firebaseapp.com",
+  projectId: "react-firebase-todo-29447",
+  storageBucket: "react-firebase-todo-29447.appspot.com",
+  messagingSenderId: "296990367642",
+  appId: "1:296990367642:web:b3d06cfc12cdbd6f7de423",
+  measurementId: "G-NM4J5S8CCZ"
+})
+
+const db = firebaseApp.firestore()
+
+export { db };
+
